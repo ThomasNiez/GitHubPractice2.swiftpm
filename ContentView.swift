26e9b64109891c5ebbde772aa677ad2eb129d21e
Thomas Niezyniecki <tmniez@comcast.net>
2023-04-06 14:09:51 -0500

    NavigationLink Fix

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            NavigationLink(destination: NounView)
+            NavigationLink("Noun") {
+                NounView()
+            }
         }
     }
 }
