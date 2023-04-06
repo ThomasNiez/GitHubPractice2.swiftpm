7a2d2b8f17eac6620e819d1505fe08dac0e6665c
Ali Ibrahim <alialchi07@gmail.com>
2023-04-06 14:21:32 -0500

    Creating Madly page and properties for noun, verb & adjective

---
+++ MadLib.swift
@@ -0,0 +1,15 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Ali Alchikh Ibrahim on 4/6/23.
+//
+
+import SwiftUI
+   
+struct MadLib {
+    var noun: String
+    var verb: String
+    var adjective: String
+}
+
